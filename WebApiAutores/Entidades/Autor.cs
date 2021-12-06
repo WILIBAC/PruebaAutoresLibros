82cb1ef822cbbb8a112ef91d0aa61fe1ea5e19a3
WYL BAUTISTA <wilibac@hotmail.com>
2021-12-05 20:16:29 -0500

    Creación primer proyecto

---
+++ WebApiAutores/Entidades/Autor.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApiAutores.Entidades
+{
+    public class Autor
+    {
+        public int Id { get; set; }
+        public string NombreCompleto { get; set; }
+        public string CiudadProcedencia { get; set; }
+        public string Correo { get; set; }
+        public List<Libro> Libros { get; set; }
+    }
+}
---
+++ WebApiAutores/Entidades/Libro.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApiAutores.Entidades
+{
+    public class Libro
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public int AutorId { get; set; }
+        public int Anio { get; set; }
+        public string Genero { get; set; }
+        public int NumeroPaginas { get; set; }
+        public Autor Autor { get; set; }
+    }
+}
